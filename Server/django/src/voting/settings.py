4e4e17bad37e48af8550c4e5f38fa86e943153b7
haniye-afshar <69324367+haniye-afshar@users.noreply.github.com>
2020-12-06 12:29:06 +0330

    Update settings.py

--- Server/django/src/voting/settings.py
+++ Server/django/src/voting/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'xaad6p*%1&-yg99zk6fvofma2t-j8*q_v74b2tg61^ab)0386o'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['meritmoney.ir','www.meritmoney.ir']
+ALLOWED_HOSTS = ['meritmoney.ir','www.meritmoney.ir','51.89.109.218']
 
 
 # Application definition
